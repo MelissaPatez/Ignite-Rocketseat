a4ed57756bfb505ddf8b4f6b2629a42f97ec3c0a
Melissa Patez (Teltec Solutions) <m.patez@teltecsolutions.com.br>
2021-12-03 19:54:01 -0300

    creating business rules and cruds

--- Node.js/primeiro-projeto/src/index.js
+++ Node.js/primeiro-projeto/src/index.js
@@ -9,6 +9,32 @@ app.use(express.json());
 //simular banco de dados
 const customers = [];
 
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next){
+    const { cpf } = request.headers;
+
+    //validando se o cpf existe
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if(!customer){
+        return response.status(400).json({error: "Customer Not Found"});
+    };
+    request.customer = customer;
+
+    return next();
+}; 
+
+// calculo saldo
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === "credit"){
+            return acc + operation.amount;
+        }else{
+            return acc - operation.amount;
+        }
+    }, 0);
+    return balance;
+}
 
 // Criar conta
 /*
@@ -30,11 +56,71 @@ app.post("/account", (request, response) => {
         cpf,
         name,
         id:uuidv4() ,
-        statement: []
+        statement: [],
     });
 
     return response.status(201).send();
 
+});
+
+//fazer deposito
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+//saque
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) =>{
+    const{ amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount){
+        return response.status(400).json({error: "Insufficient funds!"});
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+//buscar extrato       //middleware
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer.statement);
+});
+
+
+//buscar o extrato bancário do cliente por data
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+    const { date } = request.query;
+
+    const dateFormat = new Date(date + " 00:00");
+
+    //percorrer array e retornar o extrato do dia
+    const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+    return response.json(statement);
 });
 
 
